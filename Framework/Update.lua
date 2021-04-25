fa8380146e8f195fa1efdda77de94eb273c6ac06
Alexflamer2 <24377895+Alexflamer11@users.noreply.github.com>
2021-04-24 21:28:45 -0500

    Update Update.lua
    Update info

--- Framework/Update.lua
+++ Framework/Update.lua
@@ -1,3 +1,17 @@
+local CurrentLine = 1
+function Output(text)
+	term.setCursorPos(1, CurrentLine)
+	CurrentLine = CurrentLine + 1
+	term.write(text)
+end
+
+term.clear()
+term.setCursorPos(1, 1)
+term.setTextScale(1)
+term.setTextColor(colors.white)
+term.setTextBackground(colors.black)
+Output("Starting update...")
+
 -- Make sure our directory exists
 if not fs.isDir("Framework") then
 	fs.makeDir("Framework")
@@ -5,17 +19,26 @@ end
 
 -- Download a new file, returns true if success
 function DownloadFile(path, link)
-  local Result = http.get(link)
-  if Result then
-    local File = fs.open(path, "w")
-    File.write(Result.readAll())
-    File.close()
-  else
-    return false
-  end
-    
-  return true
+	local Result = http.get(link)
+	if Result then
+		local File = fs.open(path, "w")
+		File.write(Result.readAll())
+		File.close()
+	else
+		return false
+	end
+	
+	return true
 end
 
 -- Download the update file along with everything else
 DownloadFile("Framework/Update.lua", "https://raw.githubusercontent.com/Alexflamer11/ComputerCraft/master/Framework/Update.lua")
+
+Output("Update finished.")
+for i=1, 3 do
+	Output("Closing in "..tostring(i))
+	sleep(1)
+end
+
+term.clear()
+term.setCursorPos(1, 1)
