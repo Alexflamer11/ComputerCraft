39d4827d97218047850a64f58bdb14efd6ca865c
Alexflamer2 <24377895+Alexflamer11@users.noreply.github.com>
2021-04-24 21:15:00 -0500

    Create Update.lua

---
+++ Framework/Update.lua
@@ -0,0 +1,21 @@
+-- Make sure our directory exists
+if not fs.isDir("Framework") then
+	fs.makeDir("Framework")
+end
+
+-- Download a new file, returns true if success
+function DownloadFile(path, link)
+  local Result = http.get(link)
+  if Result then
+    local File = fs.open(path, "w")
+    File.write(Result.readAll())
+    File.close()
+  else
+    return false
+  end
+    
+  return true
+end
+
+-- Download the update file along with everything else
+DownloadFile("Framework/Update.lua", "https://github.com/Alexflamer11/ComputerCraft/new/master/Framework/Update.lua")
