f651ef912a679da2de71c2644842b17cc3a72962
Alexflamer2 <24377895+Alexflamer11@users.noreply.github.com>
2021-04-24 21:59:06 -0500

    Update Update.lua
    Update

--- Framework/Update.lua
+++ Framework/Update.lua
@@ -8,8 +8,8 @@ end
 term.clear()
 term.setCursorPos(1, 1)
 term.setTextColor(colors.white)
-term.setTextBackground(colors.black)
-term.write("Starting update...")
+term.setBackgroundColor(colors.black)
+Output("Starting update...")
 
 -- Make sure our directory exists
 if not fs.isDir("Framework") then
@@ -23,8 +23,10 @@ function DownloadFile(path, link)
 		local File = fs.open(path, "w")
 		File.write(Result.readAll())
 		File.close()
+		
+		Output("Downloaded \""..path.."\" successfully.")
 	else
-		Output("Unable to download \""..link.."\"")
+		Output("Unable to download \""..path.." from ("..link..")")
 		return false
 	end
 	
@@ -34,12 +36,11 @@ end
 -- Download the update file along with everything else
 DownloadFile("Framework/Update.lua", "https://raw.githubusercontent.com/Alexflamer11/ComputerCraft/master/Framework/Update.lua")
 
-for i=1, 3 do
+Output("Update finished")
+for i=3, 1, -1 do
 	Output("Closing in "..tostring(i))
 	sleep(1)
 end
 
 term.clear()
 term.setCursorPos(1, 1)
-
-loadstring(http.get("https://raw.githubusercontent.com/Alexflamer11/ComputerCraft/master/Framework/Update.lua").readAll())()
